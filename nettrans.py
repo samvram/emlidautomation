58f2c7c7b8d4f12a5131fbf4e7aca76dc757c888
samvram <samvram.iist@gmail.com>
2017-06-02 13:35:01 +0530

    Implementing few standard structures

--- nettrans.py
+++ nettrans.py
@@ -3,6 +3,37 @@ import paramiko
 import cmd
 
 class nettrans:
+    """
+       Nettrans is to get files via ftp operation over ssh protocol
+       While creation you need to specify IP, Username and Password 
+       of the client from whom you want the files.
+       
+       example use:
+             nettrans sam('192.168.3.145','root','emlidreach')
+          
+       Various modes of operation:
+       Mode latest - Fetch the latest RAW file with .UBX extension
+       Mode all - Fetch all the files present in ~/logs folder
+       Mode raws - Fetch all the RAW files in the ~/logs folder
+
+       example use:
+             nettrans sam('192.168.3.145','root','emlidreach')
+             sam.create_connection( )
+             sam.mode_select('all')
+
+       The file transfer occurs once we implement the above
+       
+       example use:
+             sam.get_file( )
+
+       You must close all connections made during the operation
+
+       example use:
+             sam.close_connection( )
+       
+       More modes to come soon
+
+    """
        
     def __init__(ip,username,password):
         self.ip=ip
@@ -10,16 +41,16 @@ class nettrans:
         self.pw=password
         self.mode=0
 
-    def modeSelect(self, x):
-        self.mode=int(x)
+    def mode_select(self, x):
+        self.mode=x
 
-    def createConnection(self):
+    def create_connection(self):
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(ip, username=uname, password=pw)
         ftp=client.open_sftp( )
 
-    def getFile(self):
+    def get_file(self):
         sam=[];
         raw=[];
         dt=[];
@@ -29,7 +60,7 @@ class nettrans:
             print('>>> ' + line.strip('\n'))
             sam.append(line.strip('\n'))
 
-        if(mode==0):
+        if(mode=='latest'):
             for i in range(0,len(sam)):
                 print(sam[i])
                 if(sam[i][0:3] == 'raw'):
@@ -60,13 +91,13 @@ class nettrans:
             #FTP Standard protocol commands to make file transfer
             ftp.get(latestfile,resultfile)
 
-        if(mode==1):
+        if(mode=='all'):
             for i in range(0,len(sam)):
                 fetchfile='logs/'+sam[i]
                 resultfile=sam[i]
                 ftp.get(sam[i],resultfile)
 
-        if(mode==2):
+        if(mode=='raws'):
             for i in range(0,len(sam)):
                 print(sam[i])
                 if(sam[i][0:3] == 'raw'):
@@ -78,7 +109,7 @@ class nettrans:
                 ftp.get(raw[i],resultfile)
 
 
-    def closeConnection(self):
+    def close_connection(self):
         client.close( )
         ftp.close( )
 
