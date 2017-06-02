016d1336da7a40d71b2a431c0f8a28c5b92b6f22
samvram <samvram.iist@gmail.com>
2017-06-02 16:54:27 +0530

    Implemented few standard procedures as asked by Nekhelesh

--- nettrans.py
+++ nettrans.py
@@ -2,6 +2,12 @@ import base64
 import paramiko
 import cmd
 
+
+class modeNotFound(Exception):
+    pass
+"""Exception for mode not being in previous list"""
+
+
 class nettrans:
     """
        Nettrans is to get files via ftp operation over ssh protocol
@@ -9,7 +15,7 @@ class nettrans:
        of the client from whom you want the files.
        
        example usage ::
-             nettrans sam('192.168.3.145','root','emlidreach')
+             nettrans object('192.168.3.145','root','emlidreach')
           
        Various modes of operation:
        Mode latest - Fetch the latest RAW file with .UBX extension
@@ -17,23 +23,30 @@ class nettrans:
        Mode raws - Fetch all the RAW files in the ~/logs folder
 
        example usage ::
-             nettrans sam('192.168.3.145','root','emlidreach')
-             sam.create_connection( )
-             sam.mode_select('all')
+             nettrans object('192.168.3.145','root','emlidreach')
+             object.create_connection( )
+             object.mode_select('all')
 
        The file transfer occurs once we implement the above
        
        example usage::
-             sam.get_file( )
+             object.get_file( )
 
        You must close all connections made during the operation
 
        example usage ::
-             sam.close_connection( )
+             object.close_connection( )
        
        More modes to come soon
 
     """
+    _modes = ['all','raws','latest']
+
+    def __enter__(self):
+        self.create_connection( )
+
+    def __exit__(self):
+        self.close_connection( )
        
     def __init__(ip,username,password):
         self.ip=ip
@@ -41,8 +54,13 @@ class nettrans:
         self.pw=password
         self.mode=0
 
+    @property
     def mode_select(self, x):
-        self.mode=x
+        if x in _modes:
+           self.mode=x
+        else
+           raise modeNotFound('The mode is not listed')
+
 
     def create_connection(self):
         client = paramiko.SSHClient()
@@ -50,21 +68,13 @@ class nettrans:
         client.connect(ip, username=uname, password=pw)
         ftp=client.open_sftp( )
 
-    def get_file(self):
-        sam=[];
+    def _latest_file_download(files):
         raw=[];
         dt=[];
-        
-        stdin, stdout, stderr = client.exec_command('ls logs')
-        for line in stdout:
-            print('>>> ' + line.strip('\n'))
-            sam.append(line.strip('\n'))
-
-        if(mode=='latest'):
-            for i in range(0,len(sam)):
-                print(sam[i])
-                if(sam[i][0:3] == 'raw'):
-                    raw.append(sam[i])
+        for i in range(0,len(files)):
+                print(files[i])
+                if(files[i][0:3] == 'raw'):
+                    raw.append(files[i])
                     
             print('\n The raw files are')
             for i in range(0,len(raw)):
@@ -91,22 +101,42 @@ class nettrans:
             #FTP Standard protocol commands to make file transfer
             ftp.get(latestfile,resultfile)
 
-        if(mode=='all'):
-            for i in range(0,len(sam)):
-                fetchfile='logs/'+sam[i]
-                resultfile=sam[i]
-                ftp.get(sam[i],resultfile)
+    def _all_files_download(files):
+        for i in range(0,len(files)):
+                fetchfile='logs/'+files[i]
+                resultfile=files[i]
+                ftp.get(fetchfile,resultfile)
 
-        if(mode=='raws'):
-            for i in range(0,len(sam)):
-                print(sam[i])
-                if(sam[i][0:3] == 'raw'):
-                    raw.append(sam[i])
+
+    def _raw_files_download(files):
+        raw=[];
+        for i in range(0,len(files)):
+                print(files[i])
+                if(files[i][0:3] == 'raw'):
+                    raw.append(files[i])
 
             for i in range(0,len(raw)):
                 fetchfile='logs/'+raw[i]
                 resultfile=raw[i]
-                ftp.get(raw[i],resultfile)
+                ftp.get(fetchfile,resultfile)
+
+
+    def get_file(self):
+        files=[];
+             
+        stdin, stdout, stderr = client.exec_command('ls logs')
+        for line in stdout:
+            print('>>> ' + line.strip('\n'))
+            files.append(line.strip('\n'))
+
+        if(mode=='latest'):
+            _latest_file_download(files)
+
+        if(mode=='all'):
+            _all_files_download(files)
+
+        if(mode=='raws'):
+            _raw_files_download(files)
 
 
     def close_connection(self):
